b008bfae457f5542bb5a5454254d028c132b12ca
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-08-16 17:41:17 +0700

    Add Data skill & link

--- Kotlin/app/src/main/java/com/ahmadabuhasan/dicoding/DetailActivity.kt
+++ Kotlin/app/src/main/java/com/ahmadabuhasan/dicoding/DetailActivity.kt
@@ -1,7 +1,10 @@
 package com.ahmadabuhasan.dicoding
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -13,6 +16,8 @@ class DetailActivity : AppCompatActivity() {
     private lateinit var imgPhoto: ImageView
     private lateinit var tvName: TextView
     private lateinit var tvDetail: TextView
+    private lateinit var tvSkill: TextView
+    private lateinit var btnLink: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,18 +25,33 @@ class DetailActivity : AppCompatActivity() {
 
         supportActionBar?.setHomeButtonEnabled(true)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.title = "Detail"
 
         imgPhoto = findViewById(R.id.img_detail_photo)
         tvName = findViewById(R.id.tv_detail_name)
         tvDetail = findViewById(R.id.tv_detail_detail)
+        tvSkill = findViewById(R.id.tv_detail_skill)
+        btnLink = findViewById(R.id.btn_detail_link)
 
         Glide.with(applicationContext)
             .load(intent.getIntExtra("image", 0))
             .apply(RequestOptions())
             .into(imgPhoto)
-        tvName.text = intent.getStringExtra("name").toString()
-        tvDetail.text = intent.getStringExtra("detail").toString()
+
+        val name = intent.getStringExtra("name").toString()
+        val detail = intent.getStringExtra("detail").toString()
+        val skill = intent.getStringExtra("skill").toString()
+        val link = intent.getStringExtra("link").toString()
+
+        tvName.text = name
+        tvDetail.text = detail
+        tvSkill.text = skill
+
+        btnLink.setOnClickListener {
+            val i = Intent(Intent.ACTION_VIEW, Uri.parse(link))
+            startActivity(i)
+        }
+
+        supportActionBar?.title = "Detail $name"
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
