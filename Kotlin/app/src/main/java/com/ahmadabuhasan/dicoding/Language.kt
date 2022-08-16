bffca1ad1da36900073efdeac6ea940866482570
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-08-16 17:38:42 +0700

    Add skill & link

--- Kotlin/app/src/main/java/com/ahmadabuhasan/dicoding/Language.kt
+++ Kotlin/app/src/main/java/com/ahmadabuhasan/dicoding/Language.kt
@@ -3,5 +3,7 @@ package com.ahmadabuhasan.dicoding
 data class Language(
     var name: String = "",
     var detail: String = "",
+    var skill: String = "",
+    var link: String = "",
     var image: Int = 0
 )
