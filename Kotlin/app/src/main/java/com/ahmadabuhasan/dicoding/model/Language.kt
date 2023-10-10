1c5fcd42c60d52c83a63f8155db66ed019c1dc78
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2023-10-10 23:28:54 +0700

    add Parcelable

--- Kotlin/app/src/main/java/com/ahmadabuhasan/dicoding/model/Language.kt
+++ Kotlin/app/src/main/java/com/ahmadabuhasan/dicoding/model/Language.kt
@@ -1,9 +1,14 @@
 package com.ahmadabuhasan.dicoding.model
 
+import android.os.Parcelable
+
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Language(
     var name: String = "",
     var detail: String = "",
     var skill: String = "",
     var link: String = "",
     var image: Int = 0
-)
+) : Parcelable
